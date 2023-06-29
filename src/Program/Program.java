b9152f40984dab41eb61ae0ed71a429551758afb
Admin <andendered@gmail.com>
2023-06-29 16:48:00 +0300

    sw, c

--- src/Program/Program.java
+++ src/Program/Program.java
@@ -3,20 +3,21 @@ package Program;
 import com.google.gson.Gson;
 
 import com.google.gson.reflect.TypeToken;
+
 import java.io.*;
 import java.util.*;
 
 
 public class Program {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         ArrayList<recipe> book = new ArrayList();
         Gson GSON = new Gson();
-        try (FileReader r = new FileReader("C:\\java\\project5-8\\info.json")){
-            book = GSON.fromJson(r, new TypeToken<ArrayList<recipe>>(){}.getType());
-        }catch(IOException e){
+        try (FileReader r = new FileReader("C:\\java\\project5-8\\info.json")) {
+            book = GSON.fromJson(r, new TypeToken<ArrayList<recipe>>() {
+            }.getType());
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
 
 
         Functions func = new Functions(book);
@@ -25,43 +26,44 @@ public class Program {
         while (true) {
             System.out.println("Command: ");
             String c = scan.nextLine();
-            if (c.equals("help") ||  c.equals(".h")) {
-                System.out.println("help(.h),add(.a),list(.l),update(.u),delete(.d),sort(.srt),search(.sr),exit(.e)");
+            switch (c) {
+                case "help":
+                    System.out.println("help(.h),add(.a),list(.l),update(.u),delete(.d),sort(.srt),search(.sr),exit(.e)");
+                    break;
+                case "add":
+                    func.add(scan);
+                    break;
+                case "list":
+                    func.list();
+                    break;
+                case "upd":
+                    func.update(scan);
+                    break;
+                case "delete":
+                    func.delete(scan);
+                    break;
+                case "sort":
+                    func.sort(scan);
+                    break;
+                case "search":
+                    func.search(scan);
+                    break;
+                case "exit":
+                    System.out.println("Зупиненно");
+                    String lp = GSON.toJson(func.book);
+                    try {
+                        FileWriter w = new FileWriter("C:\\java\\project5-8\\info.json");
+                        w.write(lp);
+                        w.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Unknown command - " + c);
+                    break;
             }
-            else if (c.equals("add") || c.equals(".a")){
-                func.add(scan);
-            }
-            else if (c.equals("list") || c.equals(".l")){
-                func.list();
-            }
-            else if (c.equals("update")||  c.equals(".u")) {
-                func.update(scan);
-            }
-            else if (c.equals("delete") || c.equals(".d")) {
-                func.delete(scan);
-            }
-            else if (c.equals("sort")||  c.equals(".srt")) {
-
-                func.sort(scan);
-            }
-
-            else if (c.equals("search") || c.equals(".sr")) {
-                func.search(scan);
-            }
-            else if (c.equals("exit") || c.equals(".e")) {
-                System.out.println("Зупиненно");
-                String lp = GSON.toJson(func.book);
-                try {
-                    FileWriter w = new FileWriter("C:\\java\\project5-8\\info.json");
-                    w.write(lp);
-                    w.close();
-                }catch (IOException e){
-                    throw new RuntimeException(e);
-                }
-                break;
-            }
-            else{
-                System.out.println("Unknown command - " + c);}
 
         }
     }
